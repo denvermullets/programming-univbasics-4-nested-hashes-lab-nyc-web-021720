01898626deba29298c303df8e8e29d5e731bac8b
Ryan Vaznis <ryan.vaznis@gmail.com>
2020-02-04 18:47:43 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,13 +16,14 @@ def hopper
         }
      }
 
+  programmer_hash[:grace_hopper]
+  
 
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
-	programmer_hash = 
+  
+	 programmer_hash = 
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -37,6 +38,9 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+  programmer_hash[:alan_kay][:known_for]
+  
 end
 
 def dennis_ritchies_language
@@ -55,6 +59,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+    
+  programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
